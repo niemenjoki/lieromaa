72f955fbc615ec941158b3672b6c30921e44b92b
Joonas Niemenjoki <46967273+niemenjoki@users.noreply.github.com>
2025-09-21 22:33:45 +0300

    Update site URL to use www domain

--- data/vars.js
+++ data/vars.js
@@ -1,5 +1,5 @@
-export const POSTS_PER_PAGE = 6;
-export const SITE_URL = 'https://luomuliero.fi';
+export const POSTS_PER_PAGE = 5;
+export const SITE_URL = 'https://www.luomuliero.fi';
 export const REPO_URL = 'https://github.com/niemenjoki/luomuliero';
 export const LICENSE_URL =
   'https://github.com/niemenjoki/luomuliero/blob/master/LICENSE.md';
