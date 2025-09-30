377b40684b1414bdfeb929d9313a6cc06ed8badd
Joonas Niemenjoki <joonas.niemenjoki@gmail.com>
2025-09-30 17:46:10 +0300

    Generate resized versions of original full size blogpost images

--- utils/generateImages.js
+++ utils/generateImages.js
@@ -6,6 +6,7 @@ const BLOGPOST_IMAGES = path.join(__dirname, '../public/images/posts');
 const WORMSPAGE_IMAGES = path.join(__dirname, '../public/images/wormspage');
 const sizes = [800, 1200];
 const formats = ['avif', 'webp', 'jpg'];
+const maxSize = Math.max(...sizes); // largest version needed
 
 // Recursively get PNG images
 function getImages(dir) {
@@ -23,23 +24,49 @@ function getImages(dir) {
 }
 
 (async () => {
-  // Generate all image sizes/formats
   const images = [
     ...getImages(BLOGPOST_IMAGES),
     ...getImages(WORMSPAGE_IMAGES),
   ];
 
   for (const imgPath of images) {
-    const { dir, name } = path.parse(imgPath);
+    const { dir, name, ext } = path.parse(imgPath);
+
+    // Generate multiple formats/sizes
     for (const size of sizes) {
       for (const format of formats) {
         const outFile = path.join(dir, `${name}-${size}.${format}`);
         if (!fs.existsSync(outFile)) {
-          console.log(`Generating: ${outFile}`);
+          console.log(`Generating: ${path.basename(outFile)}`);
           await sharp(imgPath).resize(size).toFormat(format).toFile(outFile);
         }
       }
     }
+
+    // Replace the original only in dev
+    if (process.env.NODE_ENV === 'development') {
+      const tmpFile = path.join(dir, `${name}-tmp${ext}`);
+      console.log(
+        `Replacing original with max ${maxSize}px version: ${path.basename(
+          imgPath
+        )}`
+      );
+      await sharp(imgPath)
+        .resize({
+          width: maxSize,
+          withoutEnlargement: true,
+          kernel: sharp.kernel.lanczos3,
+        })
+        .png({ compressionLevel: 9 })
+        .toFile(tmpFile);
+
+      fs.renameSync(tmpFile, imgPath);
+    } else {
+      console.log(
+        `Skipping original replacement in production: ${path.basename(imgPath)}`
+      );
+    }
   }
+
   console.log('Image generation complete.');
 })();
