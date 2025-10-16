f5d0084fc6a905bb43ed5c5f78349dd4c7346e9f
Joonas Niemenjoki <joonas.niemenjoki@gmail.com>
2025-10-16 15:08:41 +0300

    Replace switch statement with icon lookup map for cleaner, scalable icon handling

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -12,35 +12,22 @@ import {
 import { faMoon, faRss, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Icon = ({ name, ...rest }) => {
-  switch (name) {
-    case 'rss':
-      return <FontAwesomeIcon icon={faRss} {...rest} />;
-    case 'github':
-      return <FontAwesomeIcon icon={faGithub} {...rest} />;
-    case 'instagram':
-      return <FontAwesomeIcon icon={faInstagram} {...rest} />;
-    case 'quora':
-      return <FontAwesomeIcon icon={faQuora} {...rest} />;
-    case 'youtube':
-      return <FontAwesomeIcon icon={faYoutube} {...rest} />;
-    case 'twitter':
-      return <FontAwesomeIcon icon={faXTwitter} {...rest} />;
-    case 'linkedin':
-      return <FontAwesomeIcon icon={faLinkedin} {...rest} />;
-    case 'facebook':
-      return <FontAwesomeIcon icon={faFacebook} {...rest} />;
-    case 'whatsapp':
-      return <FontAwesomeIcon icon={faWhatsapp} {...rest} />;
-    case 'sun':
-      return <FontAwesomeIcon icon={faSun} {...rest} />;
-    case 'moon':
-      return <FontAwesomeIcon icon={faMoon} {...rest} />;
-    case 'tiktok':
-      return <FontAwesomeIcon icon={faTiktok} {...rest} />;
-    default:
-      return null;
-  }
+const icons = {
+  rss: faRss,
+  github: faGithub,
+  instagram: faInstagram,
+  quora: faQuora,
+  youtube: faYoutube,
+  twitter: faXTwitter,
+  linkedin: faLinkedin,
+  facebook: faFacebook,
+  whatsapp: faWhatsapp,
+  sun: faSun,
+  moon: faMoon,
+  tiktok: faTiktok,
 };
 
-export default Icon;
+export default function Icon({ name, ...rest }) {
+  const icon = icons[name];
+  return icon ? <FontAwesomeIcon icon={icon} {...rest} /> : null;
+}
