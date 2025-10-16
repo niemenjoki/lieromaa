9a1d67b2324442640aeefbd74abdca74654205b0
Joonas Niemenjoki <joonas.niemenjoki@gmail.com>
2025-10-16 15:39:38 +0300

    Add Cache-Control heading for images (immutable, 1 year)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:all*(png|jpg|jpeg|webp|avif|ico|svg)',
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }],
+      },
+    ];
+  },
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
 };
