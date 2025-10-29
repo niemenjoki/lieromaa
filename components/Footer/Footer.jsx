1c5a3f692bd6b4f50c7ffa181ad8d3f4e79b360d
Joonas Niemenjoki <joonas.niemenjoki@gmail.com>
2025-10-29 03:33:52 +0200

    Redesign Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,77 +1,90 @@
-import Link from 'next/link';
+'use client';
 
+import SafeLink from '@/components/SafeLink/SafeLink';
+import Socials from '@/components/Socials/Socials';
 import { LICENSE_URL, REPO_URL } from '@/data/vars.mjs';
 
-import Socials from '../Socials/Socials.jsx';
 import classes from './Footer.module.css';
 
-const Footer = () => {
+export default function Footer() {
+  const startYear = 2025;
+  const currentYear = new Date().getFullYear();
+  const yearRange =
+    currentYear === startYear ? `${startYear}` : `${startYear}–${currentYear}`;
+
   return (
     <footer className={classes.Footer}>
-      <div className={classes.Left}>
-        <ul className={classes.Links}>
-          <li>
-            <Link href={'/blogi'}>Blogi</Link>
-          </li>
-          <li>
-            <Link href={'/tuotteet/madot'}>Osta kompostimatoja</Link>
-          </li>
-          <li>
-            <Link href={'/matolaskuri'}>Kompostimatojen laskuri</Link>
-          </li>
-
-          <li>
-            <Link href={'/tietoa'}>Tietoa sivustosta</Link>
-          </li>
-          <li>
-            <Link href={'/tietosuoja'}>Tietosuoja</Link>
-          </li>
-          <li>
-            <Link href={'/tilausehdot'}>Tilausehdot</Link>
-          </li>
-        </ul>
-
-        <div className={classes.NoMobile}>
+      <div className={classes.Inner}>
+        <div className={classes.Columns}>
           <div>
-            &copy; 2025
-            {new Date().getFullYear() > 2025 && `-${new Date().getFullYear()}`} Lieromaa |
-            Joonas Niemenjoki. Koodi ja sisältö on
-            <a href={LICENSE_URL} target="_blank" rel="license noopener noreferrer">
-              {' '}
-              lisensoitu
-            </a>
+            <h3>Tuotteet</h3>
+            <ul>
+              <li>
+                <SafeLink href="/tuotteet/madot">Kompostimadot</SafeLink>
+              </li>
+              <li>
+                {/*<SafeLink href="/tuotteet/matokompostin-aloituspakkaus">Aloituspakkaus</SafeLink>*/}
+              </li>
+            </ul>
           </div>
-          <div></div>
+
+          {/*<div>
+            <h3>Oppaat</h3>
+            <ul>
+              <li>
+                <SafeLink href="/opas/kompostorin-perustaminen">
+                  Kompostorin perustaminen
+                </SafeLink>
+              </li>
+              <li>
+                <SafeLink href="/opas/kompostorin-hoito">Kompostorin hoito</SafeLink>
+              </li>
+              <li>
+                <SafeLink href="/opas/matokakan-kerays">Matokakan keräys</SafeLink>
+              </li>
+            </ul>
+          </div>*/}
+
+          <div>
+            <h3>Sivut</h3>
+            <ul>
+              <li>
+                <SafeLink href="/blogi">Blogi</SafeLink>
+              </li>
+              <li>
+                <SafeLink href="/matolaskuri">Matolaskuri</SafeLink>
+              </li>
+              <li>
+                <SafeLink href="/tietoa">Tietoa</SafeLink>
+              </li>
+              <li>
+                <SafeLink href="/tietosuoja">Tietosuoja</SafeLink>
+              </li>
+              <li>
+                <SafeLink href="/tilausehdot">Tilausehdot</SafeLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className={classes.Socials}>
+            <h3>Seuraa</h3>
+            <Socials />
+          </div>
         </div>
-      </div>
-      <div className={classes.Right}>
-        <ul className={classes.Links}>
-          <li>
-            <a target="_blank" rel="noreferrer" href={REPO_URL}>
+
+        <div className={classes.Bottom}>
+          <p>&copy; {yearRange} Joonas Niemenjoki</p>
+          <p className={classes.SafeLinks}>
+            <a href={LICENSE_URL} target="_blank" rel="noopener noreferrer">
+              Lisenssi
+            </a>
+            {' | '}
+            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
               Lähdekoodi
             </a>
-          </li>
-          <li className={classes.Socials}>
-            <Socials />
-          </li>
-        </ul>
-      </div>
-      <div className={classes.Mobile}>
-        <div>
-          &copy; 2025
-          {new Date().getFullYear() > 2025 && `-${new Date().getFullYear()}`} Lieromaa |
-          Joonas Niemenjoki
-        </div>
-        <div>
-          Koodi ja sisältö on
-          <a href={LICENSE_URL} target="_blank" rel="license noopener noreferrer">
-            {' '}
-            lisensoitu
-          </a>
+          </p>
         </div>
       </div>
     </footer>
   );
-};
-
-export default Footer;
+}
