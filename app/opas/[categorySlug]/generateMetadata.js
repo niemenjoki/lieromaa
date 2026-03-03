ebfeb7157ff3e4def1554fbb2c02d8116fcb113d
Joonas Niemenjoki <joonas.niemenjoki@gmail.com>
2026-03-03 14:45:49 +0200

    Fix typo

--- app/opas/[categorySlug]/generateMetadata.js
+++ app/opas/[categorySlug]/generateMetadata.js
@@ -3,7 +3,7 @@ import { withDefaultMetadata } from '@/lib/metadata/withDefaultMetadata';
 export default async function generateMetadata({ params }) {
   const { categorySlug } = await params;
   const category = categorySlug.replaceAll('-', ' ');
-  const title = `Lieromaan oppaat aiheeesta: ${category}`;
+  const title = `Lieromaan oppaat aiheesta: ${category}`;
   const description = `Lue käytännön vinkit ja ohjeet aiheesta ${category} – miten perustaa, hoitaa ja hyödyntää matokompostia.`;
   const pageURL = `/opas/${categorySlug}`;
 
