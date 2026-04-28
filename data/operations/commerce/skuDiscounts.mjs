b2523b894577b26ddcee5c409b894ab4ddbbe3d0
Joonas Niemenjoki <joonas.niemenjoki@gmail.com>
2026-04-28 18:18:53 +0300

    Clear automatic SKU discounts

--- data/operations/commerce/skuDiscounts.mjs
+++ data/operations/commerce/skuDiscounts.mjs
@@ -2,36 +2,7 @@
 // type: 'percentage' or 'fixed'
 // value: discount amount to subtract, stored as a positive number.
 
-const skuDiscounts = {
-  'worms-25': {
-    active: true,
-    type: 'percentage',
-    value: 10,
-    lowestPrice30Days: 18,
-    validUntil: '2026-04-30',
-  },
-  'worms-50': {
-    active: true,
-    type: 'percentage',
-    value: 10,
-    lowestPrice30Days: 27,
-    validUntil: '2026-04-30',
-  },
-  'worms-75': {
-    active: true,
-    type: 'percentage',
-    value: 10,
-    lowestPrice30Days: 36,
-    validUntil: '2026-04-30',
-  },
-  'worms-100': {
-    active: true,
-    type: 'percentage',
-    value: 10,
-    lowestPrice30Days: 45,
-    validUntil: '2026-04-30',
-  },
-};
+const skuDiscounts = {};
 
 export const codelessSkuDiscounts = skuDiscounts;
 
